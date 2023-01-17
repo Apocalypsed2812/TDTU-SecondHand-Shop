fe291b2deb96a4db6bb9ffe7f7b083d4728c815c
Apocalysed <phamhuynhanhtien.12a20.2019@gmail.com>
2023-01-17 10:37:59 +0700

    add modal post detail

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,20 +1,29 @@
-import { faBars, faChevronDown, faEllipsis } from '@fortawesome/free-solid-svg-icons';
+import {
+    faBars,
+    faChevronDown,
+    faEllipsis,
+    faCircleXmark,
+    faCaretLeft,
+    faCaretRight,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
 import styles from './Home.module.scss';
-// import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 // import { getMethod } from '~/utils/fetchData';
 import avatar from '~/assets/images/avatar_post.jpg';
-import { TOKEN_NAME } from '~/credentials';
+// import { TOKEN_NAME } from '~/credentials';
 
 const cx = classNames.bind(styles);
 
 function Home() {
-    // const [productList, setProductList] = useState([]);
+    const [showModalPost, setShowModalPost] = useState(false);
 
-    // const quantityProduct = 6;
+    const hideModalPost = () => {
+        setShowModalPost(false);
+    };
 
     // useEffect(() => {
     //     const getProducts = async () => {
@@ -80,13 +89,58 @@ function Home() {
                         <p>Price : XXXXX VND</p>
                         <p>Merchandise type: Mertirial</p>
                     </div>
-                    <div className={cx('posts__button')}>
-                        <Link to="/post_detail" className={cx('posts__button-link')}>
-                            View Detail
-                        </Link>
+                    <div className={cx('posts__button')} onClick={() => setShowModalPost(true)}>
+                        <p className={cx('posts__button-link')}>View Detail</p>
                     </div>
                 </div>
             </div>
+
+            {showModalPost && (
+                <div className={cx('modal')}>
+                    <div className={cx('modal__overlay')} onClick={hideModalPost}></div>
+                    <div className={cx('modal__body')}>
+                        <div className={cx('modal__container')}>
+                            <div className={cx('wrapper')}>
+                                <div className={cx('wrapper__icon')} onClick={hideModalPost}>
+                                    <FontAwesomeIcon icon={faCircleXmark} className={cx('wrapper__icon-close')} />
+                                </div>
+                                <div className={cx('wrapper__content')}>
+                                    <div className={cx('wrapper__content-img')}>
+                                        <img src={avatar} alt="" />
+                                        <FontAwesomeIcon
+                                            icon={faCaretLeft}
+                                            className={cx('wrapper__content-img-left')}
+                                        />
+                                        <FontAwesomeIcon
+                                            icon={faCaretRight}
+                                            className={cx('wrapper__content-img-right')}
+                                        />
+                                    </div>
+                                    <div className={cx('wrapper__content-text')}>
+                                        <div className={cx('wrapper__content-avatar')}>
+                                            <img src={avatar} alt="" />
+                                            <p>Trần Thái Bảo</p>
+                                        </div>
+                                        <div className={cx('wrapper__content-time')}>
+                                            <span>19:30</span>
+                                            <span>25/12/2022</span>
+                                        </div>
+                                        <p className={cx('wrapper__content-title')}>Post's Title</p>
+                                        <p className={cx('wrapper__content-category')}>Merchandise Type</p>
+                                        <p className={cx('wrapper__content-price')}>12.000.000</p>
+                                        <p className={cx('wrapper__content-description')}>
+                                            Vai trò của báo cáo tài chính là cung cấp những thông tin hữu ích của công
+                                            ty đối thủ, tình hình tài chính và những thay đổi trong tình hình tài chính
+                                            cho các đối tượng sử dụng thông tin như nhà đầu tư, nhà cung cấp, người cho
+                                            vay và các bên liên quan khác
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </>
     );
 }
